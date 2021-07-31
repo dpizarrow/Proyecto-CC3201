7093efd7e1d08e8cf29f637bafa278fb8fac699b
dpizarrow <diego.pizarro0@gmail.com>
2021-07-31 12:45:09 -0400

    Edit forms

--- prueba/forms.py
+++ prueba/forms.py
@@ -1,11 +1,14 @@
 from django import forms
 
+
 class NombreMesForm(forms.Form):
     nombre_mes = forms.CharField(label='Nombre del mes', max_length=100)
 
 class consulta1form(forms.Form):
-    lat = forms.CharField(label='latitud', max_length=100)
-    lon = forms.CharField(label='longitud', max_length=100)
+    lat = forms.CharField(label = 'latitud', max_length = 100)
+    lon = forms.CharField(label = 'longitud', max_length = 100)
+    lat_hol = forms.CharField(label = 'holgura latitud', max_length = 100)
+    lon_hol = forms.CharField(label= 'holgura longitud', max_length = 100)
 
 class consulta2form(forms.Form):
     comentario= forms.CharField(label='comentario', max_length=100)
